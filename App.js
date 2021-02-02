3fd1a777f97f7fc8b7fa971599dced994358eaed
Roman Yatsynych <rom.yatsynych@gmail.com>
2021-02-02 14:44:55 +0200

    Added documentation for project + Heroku changes + added README

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-require('dotenv').config();
+
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger.json')
 
 const app = express()
 
@@ -10,7 +12,10 @@ const routes = require('./router')
 app.use(cors({origin: '*'}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.use(routes)
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT)
---
+++ README.md
@@ -0,0 +1,9 @@
+# Genuisee Test Task
+## Crud for working with external API
+
+1. To start with npm simply: 
+```
+npm install
+npm start
+```
+2. Or try app on [Heroku](http://https://genuiseetesttask.herokuapp.com/doc)
